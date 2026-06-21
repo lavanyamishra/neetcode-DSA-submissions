84bc0ac60420376d6da017611eb9927d72291e57
lavanyamishra <100487476+lavanyamishra@users.noreply.github.com>
2026-06-21 10:20:56 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        s = s.replace(/[^a-z0-9]/gi, '').toLowerCase();
+        let i = 0;
+        let j = s.length - 1;
+        while (i < j) {
+            console.log(s[i] , s[j])
+            if (s[i] != s[j]) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
