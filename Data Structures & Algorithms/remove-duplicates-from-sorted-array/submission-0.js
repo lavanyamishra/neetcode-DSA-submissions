25e4ec0200566977a813db737d59e230d04e3504
lavanyamishra <100487476+lavanyamishra@users.noreply.github.com>
2026-06-22 08:45:30 +0530

    Add: remove-duplicates-from-sorted-array - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-0.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    removeDuplicates(nums) {
+        let count = 0;
+        for(let i = 0;i<nums.length;i++){
+            if(nums[i] != nums[i+1]){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;
+    }
+}
