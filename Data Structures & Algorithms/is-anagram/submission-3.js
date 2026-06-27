54a95f1ceb92b7cde3c2ad4974a4e69351143c14
lavanyamishra <100487476+lavanyamishra@users.noreply.github.com>
2026-06-27 23:23:04 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+let newS = s;
+        let newT = t;
+        let sortedStringS = newS.split("").sort().join("");
+        let sortedStringT = newT.split("").sort().join("");
+        console.log(sortedStringS,sortedStringT)
+        if (sortedStringS === sortedStringT) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
